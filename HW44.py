00f45dfd0b8e768409ded90b602c7c2b82ca3cef
James Yoon <42917557+ydyoon2@users.noreply.github.com>
2019-10-08 18:45:13 -0500

    Update HW44.py

--- HW44.py
+++ HW44.py
@@ -1,91 +1,17 @@
-"""
-resnet_cifar100.py
-"""
-import torch 
-import torchvision 
-import torch.nn as nn
-import torch.utils.data 
-import torch.optim as optim
-from torch.autograd import Variable
-from torchvision import datasets
-import torchvision.transforms as transforms
-import os
-
-# Load CIFAR100 dataset
-batch_size = 256
-num_epochs = 100
-
-transform_train = transforms.Compose([transforms.ToTensor(), 
-                                      transforms.Normalize((0.4914, 0.48216, 0.44653), (0.24703, 0.24349, 0.26159))])
-transform_test = transforms.Compose([transforms.ToTensor(),
-                                     transforms.Normalize((0.4914, 0.48216, 0.44653), (0.24703, 0.24349, 0.26159))])
-# For trainning data
-trainset = torchvision.datasets.CIFAR100(root='~/scratch/', train=True,download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=8)
-# For testing data
-testset = torchvision.datasets.CIFAR100(root='~/scratch/', train=False,download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=8)
-
-# Access the data
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-for epoch in range(num_epochs):
-    for batch_idx, (images, labels) in enumerate(trainloader):
-        images = images.to(device)
-        labels = labels.to(device)
-        
-def conv3x3(in_channels, out_channels, stride=1):
-    
-    return nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-
-
-class BasicBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, downsample = None):
-        super(BasicBlock, self).__init__()
-        
-        self.conv1 = conv3x3(in_channels, out_channels, stride)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace = True)
-        
-        self.conv2 = conv3x3(out_channels, out_channels)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        
-        self.downsample = downsample
-        self.stride = stride
-            
-        
-    def forward(self, x):
-        identity = x
-        
-        out = self.conv1(x) # 3*3, stride = 1
-        out = self.bn1(out)
-        out = self.relu(out)
-        
-        out = self.conv2(out) # 3*3, stride = 1
-        out = self.bn2(out)
-        
-        if self.downsample is not None: #downsample: when the shape of identity != shape of out
-            identity = self.downsample(x)
-            
-        out += identity
-        out = self.relu(out)
-        
-        return out
-        
-    
 class ResNet(nn.Module):
     def __init__(self, basic_block, num_blocks, num_classes):
         super(ResNet, self).__init__()
 
         self.in_channels = 32
-        self.conv1 = conv3x3(3, 32, 3, 1, 1) #input, output, kernel, stride, padding
+        self.conv1 = conv3x3(3, 32, 3) #input, output, kernel, stride, padding
         self.bn1 = nn.BatchNorm2d(32) #feature
         self.relu = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout2d(p=0.02)
         
-        self.layer1 = self._make_layer(basic_block, 32, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(basic_block, 64, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(basic_block, 128, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(basic_block, 256, num_blocks[3], stride=2)
+        self.layer1 = self._make_layer(basic_block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(basic_block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(basic_block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(basic_block, 512, num_blocks[3], stride=2)
         
         self.maxpool = nn.MaxPool2d(kernel_size=4, stride=1)
         self.fc_layer = nn.Linear(256, num_classes)
@@ -132,100 +58,6 @@ class ResNet(nn.Module):
         x = self.layer4(x)
         
         x = self.maxpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc_layer(x)
+        x = self.fc(x)
         
         return x
-
-
-resnet50 = ResNet(BasicBlock, [2, 4, 4, 2], 10).to(device)
-
-criterion = nn.CrossEntropyLoss().to(device)
-optimizer = torch.optim.SGD(resnet50.parameters(), lr = 0.1, momentum = 0.9, weight_decay=5e-4)
-lr_sche = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
-
-import visdom
-
-vis = visdom.Visdom()
-vis.close(env="main")
-
-loss_plt = vis.line(Y=torch.Tensor(1).zero_(),opts=dict(title='loss_tracker', legend=['loss'], showlegend=True))
-acc_plt = vis.line(Y=torch.Tensor(1).zero_(),opts=dict(title='Accuracy', legend=['Acc'], showlegend=True))
-
-def acc_check(net, test_set, epoch, save=1):
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for data in test_set:
-            images, labels = data
-            images = images.to(device)
-            labels = labels.to(device)
-            outputs = net(images)
-
-            _, predicted = torch.max(outputs.data, 1)
-
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    
-    acc = (100 * correct / total)
-    print('Accuracy of the network on the 10000 test images: %d %%' % acc)
-    if save:
-        torch.save(net.state_dict(), "./model/model_epoch_{}_acc_{}.pth".format(epoch, int(acc)))
-    return acc
-
-print(len(trainloader))
-epochs = 150
-
-for epoch in range(epochs):  # loop over the dataset multiple times
-
-    running_loss = 0.0
-    lr_sche.step()
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs
-        inputs, labels = data
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = resnet50(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 30 == 29:    # print every 30 mini-batches
-            value_tracker(loss_plt, torch.Tensor([running_loss/30]), torch.Tensor([i + epoch*len(trainloader) ]))
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 30))
-            running_loss = 0.0
-    
-    #Check Accuracy
-    acc = acc_check(resnet50, testloader, epoch, save=1)
-    value_tracker(acc_plt, torch.Tensor([acc]), torch.Tensor([epoch]))
-    
-
-print('Finished Training')
-
-correct = 0
-total = 0
-
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = resnet50(images)
-        
-        _, predicted = torch.max(outputs.data, 1)
-        
-        total += labels.size(0)
-        
-        correct += (predicted == labels).sum().item()
-
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
-
