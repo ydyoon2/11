f87c76a60c88806682fadc4af940efe90c291c52
James Yoon <42917557+ydyoon2@users.noreply.github.com>
2019-10-09 14:41:13 -0500

    Update HW44.py

--- HW44.py
+++ HW44.py
@@ -1,26 +1,31 @@
-import os
 import torch
 import torchvision
 import torch.optim
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import torchvision.transforms as transforms
-import argparse
 import torch.utils.data
 from torch.autograd import Variable
+import argparse
 
-def data_loader(dataroot, batch_size_train, batch_size_test):    
-    transform_train = transforms.Compose([transforms.RandomCrop(size=32, padding=4),
-                                          transforms.RandomVerticalFlip(),
-                                          transforms.ToTensor(), 
-                                          transforms.Normalize((0.4914, 0.48216, 0.44653), (0.24703, 0.24349, 0.26159))])
-    transform_test = transforms.Compose([transforms.ToTensor(), 
-                                         transforms.Normalize((0.4914, 0.48216, 0.44653), (0.24703, 0.24349, 0.26159))])
-    trainset = torchvision.datasets.CIFAR100(root='~/scratch/', train=True, download=True, transform=transform_train)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size_train, shuffle=True, num_workers=8)
-    testset = torchvision.datasets.CIFAR100(root='~/scratch/', train=False, download=True, transform=transform_test)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size_test, shuffle=False, num_workers=8)
-    return trainloader, testloader
+# CIFAR100
+transform_train = transforms.Compose([transforms.RandomCrop(size=32, padding=4),
+                                      transforms.RandomVerticalFlip(),
+                                      transforms.ToTensor(), 
+                                      transforms.Normalize((0.4914, 0.48216, 0.44653), (0.24703, 0.24349, 0.26159))])
+transform_test = transforms.Compose([transforms.ToTensor(), 
+                                     transforms.Normalize((0.4914, 0.48216, 0.44653), (0.24703, 0.24349, 0.26159))])
+trainset = torchvision.datasets.CIFAR100(root='~/scratch/', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True, num_workers=8)
+testset = torchvision.datasets.CIFAR100(root='~/scratch/', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=256, shuffle=False, num_workers=8)
+
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+
+def resnet_cifar(**kwargs):
+    model = ResNet(BasicBlock, [2, 4, 4, 2], 100, **kwargs)
+    return model
 
 def initialize_weights(module):
     if isinstance(module, nn.Conv2d):
@@ -30,14 +35,6 @@ def initialize_weights(module):
         module.bias.data.zero_()
     elif isinstance(module, nn.Linear):
         module.bias.data.zero_()
-
-def conv3x3(in_channels, out_channels, stride=1):
-    return nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-
-def resnet_cifar(**kwargs):
-    model = ResNet(BasicBlock, [2, 4, 4, 2], 100, **kwargs)
-    return model
-
 
 class BasicBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
@@ -69,34 +66,34 @@ class BasicBlock(nn.Module):
         
         return out
 
-
 class ResNet(nn.Module):
-    def __init__(self, basic_block, num_blocks, num_classes):
+    def __init__(self, basic_block, num_blocks, num_classes=100):
         super(ResNet, self).__init__()
 
         self.in_channels = 32
-        self.conv1 = conv3x3(3, 32)
-        self.bn1 = nn.BatchNorm2d(32) #feature
+        self.conv1 = conv3x3(in_channels=3, out_channels=32)
+        self.bn = nn.BatchNorm2d(num_features=32)
         self.relu = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout2d(p=0.02)
-        
-        self.conv2_x = self._make_block(basic_block, 32, num_blocks[0], stride=1)
-        self.conv3_x = self._make_block(basic_block, 64, num_blocks[1], stride=2)
-        self.conv4_x = self._make_block(basic_block, 128, num_blocks[2], stride=2)
-        self.conv5_x = self._make_block(basic_block, 256, num_blocks[3], stride=2)
-        
+
+        self.conv2_x = self._make_block(basic_block, num_blocks[0], out_channels=32, stride=1, padding=1)
+        self.conv3_x = self._make_block(basic_block, num_blocks[1], out_channels=64, stride=2, padding=1)
+        self.conv4_x = self._make_block(basic_block, num_blocks[2], out_channels=128, stride=2, padding=1)
+        self.conv5_x = self._make_block(basic_block, num_blocks[3], out_channels=256, stride=2, padding=1)
+
         self.maxpool = nn.MaxPool2d(kernel_size=4, stride=1)
         self.fc_layer = nn.Linear(256, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.kaiming_normal_(m.weight.data, mode='fan_out')
             elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
 
-
-    def _make_block(self, basic_block, num_blocks, out_channels, stride=1):
+    def _make_block(self, basic_block, num_blocks, out_channels, stride=1, padding = 1):
         downsample = None
         if (stride != 1) or (self.in_channels != out_channels):
             downsample = nn.Sequential(
@@ -113,34 +110,33 @@ class ResNet(nn.Module):
 
         return nn.Sequential(*layers)
 
-
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.dropout(x)
+        out = self.conv1(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.dropout(out)
 
-        x = self.conv2_x(x)
-        x = self.conv3_x(x)
-        x = self.conv4_x(x)
-        x = self.conv5_x(x)
+        out = self.conv2_x(out)
+        out = self.conv3_x(out)
+        out = self.conv4_x(out)
+        out = self.conv5_x(out)
 
-        x = self.maxpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc_layer(x)
+        out = self.maxpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc_layer(out)
 
-        return x
+        return out
 
 
-def calculate_accuracy(net, loader, is_gpu):
+def calculate_accuracy(net, loader):
     correct = 0.
     total = 0.
 
     for data in loader:
         images, labels = data
-        if is_gpu:
-            images = images.cuda()
-            labels = labels.cuda()
+        
+        images = images.cuda()
+        labels = labels.cuda()
         outputs = net(Variable(images))
         _, predicted = torch.max(outputs.data, 1)
 
@@ -149,9 +145,7 @@ def calculate_accuracy(net, loader, is_gpu):
 
     return 100 * correct / total
 
-
-def train(net, criterion, optimizer, trainloader,
-          testloader, start_epoch, epochs, is_gpu):
+def train(net, criterion, optimizer, trainloader, testloader, start_epoch, epochs):
 
     for epoch in range(start_epoch, epochs + start_epoch):
 
@@ -160,9 +154,9 @@ def train(net, criterion, optimizer, trainloader,
 
             inputs, labels = data
 
-            if is_gpu:
-                inputs = inputs.cuda()
-                labels = labels.cuda()
+
+            inputs = inputs.cuda()
+            labels = labels.cuda()
 
             inputs, labels = Variable(inputs), Variable(labels)
 
@@ -178,11 +172,10 @@ def train(net, criterion, optimizer, trainloader,
 
         running_loss /= len(trainloader)
 
-        train_accuracy = calculate_accuracy(net, trainloader, is_gpu)
-        test_accuracy = calculate_accuracy(net, testloader, is_gpu)
+        train_accuracy = calculate_accuracy(net, trainloader)
+        test_accuracy = calculate_accuracy(net, testloader)
 
-        print("Iteration: {0} | Loss: {1} | Training accuracy: {2}% | Test accuracy: {3}%".format(
-            epoch+1, running_loss, train_accuracy, test_accuracy))
+        print("epoch: {}, train_accuracy: {}%, test_accuracy: {}%".format(epoch, train_accuracy, test_accuracy))
 
 
 parser = argparse.ArgumentParser()
@@ -209,64 +202,24 @@ parser.add_argument('--batch_size_test', type=int,
 # training settings
 parser.add_argument('--resume', type=bool, default=False,
                     help='whether re-training from ckpt')
-parser.add_argument('--is_gpu', type=bool, default=True,
-                    help='whether training using GPU')
+
 
 # parse the arguments
 args = parser.parse_args()
 
-
-def main():
-    """Main pipeline for training ResNet model on CIFAR100 Dataset."""
-    start_epoch = 0
-
-    # resume training from the last time
-    if args.resume:
-        # Load checkpoint
-        print('==> Resuming from checkpoint ...')
-        assert os.path.isdir(
-            '~/checkpoint'), 'Error: no checkpoint directory found!'
-        checkpoint = torch.load(args.ckptroot)
-        net = checkpoint['net']
-        start_epoch = checkpoint['epoch']
-    else:
-        # start over
-        print('==> Building new ResNet model ...')
-        net = resnet_cifar()
-
-    print("==> Initialize CUDA support for ResNet model ...")
-
-    # For training on GPU, we need to transfer net and data onto the GPU
-    # http://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html#training-on-gpu
-    if args.is_gpu:
-        # net = net.cuda()
-        # net = torch.nn.DataParallel(
-        #     net, device_ids=range(torch.cuda.device_count()))
-        net = torch.nn.DataParallel(net).cuda()
-        cudnn.benchmark = True
-
-    # Loss function, optimizer and scheduler
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(net.parameters(),
-                                lr=args.lr,
-                                momentum=args.momentum,
-                                weight_decay=args.weight_decay)
-
-    # Load CIFAR100
-    trainloader, testloader = data_loader(args.dataroot,
-                                          args.batch_size_train,
-                                          args.batch_size_test)
-
-    # training
-    train(net,
-          criterion,
-          optimizer,
-          trainloader,
-          testloader,
-          start_epoch,
-          args.epochs,
-          args.is_gpu)
+start_epoch = 0
 
 
-if __name__ == '__main__':
-    main()
+print('==> Building new ResNet model ...')
+net = resnet_cifar()
+
+print("==> Initialize CUDA support for ResNet model ...")
+
+
+net = torch.nn.DataParallel(net).cuda()
+cudnn.benchmark = True
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+
+train(net, criterion, optimizer, trainloader, testloader, start_epoch, args.epochs)
