18ccf66e553651ac88d0994aeadb6518dcb2c44d
James Yoon <42917557+ydyoon2@users.noreply.github.com>
2019-10-09 13:09:35 -0500

    Update HW4.py

--- HW4.py
+++ HW4.py
@@ -126,7 +126,7 @@ class ResNet(nn.Module):
         x = self.conv5_x(x)
 
         x = self.maxpool(x)
-        x = x.view(x.size(0), -1)
+        #x = x.view(x.size(0), -1)
         x = self.fc_layer(x)
 
         return x
