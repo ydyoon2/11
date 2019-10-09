f0ddeb6e4037763a852cfc503b089a5a5e0367c7
James Yoon <42917557+ydyoon2@users.noreply.github.com>
2019-10-08 22:40:37 -0500

    Update HW44.py

--- HW44.py
+++ HW44.py
@@ -5,221 +5,16 @@ import torch.optim
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import torchvision.transforms as transforms
-
 import argparse
-
-#from resnet import resnet_cifar
-
-
-
 import torch.utils.data
-
-
 from torch.autograd import Variable
 
-
-
-def initialize_weights(module):
-    """Initialize weights."""
-    if isinstance(module, nn.Conv2d):
-        nn.init.xavier_normal(module.weight.data)
-    elif isinstance(module, nn.BatchNorm2d):
-        module.weight.data.fill_(1)
-        module.bias.data.zero_()
-    elif isinstance(module, nn.Linear):
-        module.bias.data.zero_()
-
-
-def conv3x3(in_channels, out_channels, stride=1):
-    """3x3 kernel size with padding convolutional layer in ResNet BasicBlock."""
-    return nn.Conv2d(
-        in_channels=in_channels,
-        out_channels=out_channels,
-        kernel_size=3,
-        stride=stride,
-        padding=1,
-        bias=False)
-
-
-def resnet_cifar(**kwargs):
-    """Initialize ResNet model."""
-    model = ResNet(BasicBlock, [2, 4, 4, 2], **kwargs)
-    return model
-
-
-class BasicBlock(nn.Module):
-    """Basic Block of ReseNet."""
-
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        """Basic Block of ReseNet Builder."""
-        super(BasicBlock, self).__init__()
-
-        # First conv3x3 layer
-        self.conv1 = conv3x3(in_channels, out_channels, stride)
-
-        #  Batch Normalization
-        self.bn1 = nn.BatchNorm2d(num_features=out_channels)
-
-        # ReLU Activation Function
-        self.relu = nn.ReLU(inplace=True)
-
-        # Second conv3x3 layer
-        self.conv2 = conv3x3(out_channels, out_channels)
-
-        #  Batch Normalization
-        self.bn2 = nn.BatchNorm2d(num_features=out_channels)
-
-        # downsample for `residual`
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        """Forward Pass of Basic Block."""
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        return out
-
-
-class ResNet(nn.Module):
-    """Residual Neural Network."""
-
-    def __init__(self, block, duplicates, num_classes=100):
-        """Residual Neural Network Builder."""
-        super(ResNet, self).__init__()
-
-        self.in_channels = 32
-        self.conv1 = conv3x3(in_channels=3, out_channels=32)
-        self.bn = nn.BatchNorm2d(num_features=32)
-        self.relu = nn.ReLU(inplace=True)
-        self.dropout = nn.Dropout2d(p=0.02)
-
-        # block of Basic Blocks
-        self.conv2_x = self._make_block(block, duplicates[0], out_channels=32, stride=1, padding=1)
-        self.conv3_x = self._make_block(block, duplicates[1], out_channels=64, stride=2, padding=1)
-        self.conv4_x = self._make_block(block, duplicates[2], out_channels=128, stride=2, padding=1)
-        self.conv5_x = self._make_block(block, duplicates[3], out_channels=256, stride=2, padding=1)
-
-        self.maxpool = nn.MaxPool2d(kernel_size=4, stride=1)
-        self.fc_layer = nn.Linear(256, num_classes)
-
-        # initialize weights
-        # self.apply(initialize_weights)
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal(m.weight.data, mode='fan_out')
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                m.bias.data.zero_()
-
-    def _make_block(self, block, duplicates, out_channels, stride=1, padding = 1):
-        """
-        Create Block in ResNet.
-
-        Args:
-            block: BasicBlock
-            duplicates: number of BasicBlock
-            out_channels: out channels of the block
-
-        Returns:
-            nn.Sequential(*layers)
-        """
-        downsample = None
-        if (stride != 1) or (self.in_channels != out_channels):
-            downsample = nn.Sequential(
-                conv3x3(self.in_channels, out_channels, stride=stride),
-                nn.BatchNorm2d(num_features=out_channels)
-            )
-
-        layers = []
-        layers.append(
-            block(self.in_channels, out_channels, stride, downsample))
-        self.in_channels = out_channels
-        for _ in range(1, duplicates):
-            layers.append(block(out_channels, out_channels))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        """Forward pass of ResNet."""
-        out = self.conv1(x)
-        out = self.bn(out)
-        out = self.relu(out)
-        out = self.dropout(out)
-
-        # Stacked Basic Blocks
-        out = self.conv2_x(out)
-        out = self.conv3_x(out)
-        out = self.conv4_x(out)
-        out = self.conv5_x(out)
-
-        out = self.maxpool(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc_layer(out)
-
-        return out
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def data_loader(dataroot, batch_size_train, batch_size_test):
-    """
-    Data Loader for CIFAR100 Dataset.
-
-    Args:
-        dataroot: data root directory
-        batch_size_train: mini-Batch size of training set
-        batch_size_test: mini-Batch size of test set
-
-    Returns:
-        trainloader: training set loader
-        testloader: test set loader
-    """
-    # Data Augmentation
-    print("==> Data Augmentation ...")
-
-    # Normalize training set together with augmentation
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276])
-    ])
-
-    # Normalize test set same as training set without augmentation
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276])
-    ])
-
-    # Loading CIFAR100
-    print("==> Preparing CIFAR100 dataset ...")
+    
+    transform_train = transforms.Compose([transforms.ToTensor(), 
+                                          transforms.Normalize((0.4914, 0.48216, 0.44653), (0.24703, 0.24349, 0.26159))])
+    transform_test = transforms.Compose([transforms.ToTensor(), 
+                                         transforms.Normalize((0.4914, 0.48216, 0.44653), (0.24703, 0.24349, 0.26159))])
 
     trainset = torchvision.datasets.CIFAR100(root='~/scratch/',
                                              train=True,
@@ -238,17 +33,127 @@ def data_loader(dataroot, batch_size_train, batch_size_test):
     return trainloader, testloader
 
 
+def initialize_weights(module):
+    if isinstance(module, nn.Conv2d):
+        nn.init.xavier_normal(module.weight.data)
+    elif isinstance(module, nn.BatchNorm2d):
+        module.weight.data.fill_(1)
+        module.bias.data.zero_()
+    elif isinstance(module, nn.Linear):
+        module.bias.data.zero_()
+
+
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=3,
+        stride=stride,
+        padding=1,
+        bias=False)
+
+
+def resnet_cifar(**kwargs):
+    model = ResNet(BasicBlock, [2, 4, 4, 2], **kwargs)
+    return model
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(num_features=out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(num_features=out_channels)
+
+        self.downsample = downsample
+        self.stride = stride
+
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, duplicates, num_classes=100):
+        super(ResNet, self).__init__()
+
+        self.in_channels = 32
+        self.conv1 = conv3x3(in_channels=3, out_channels=32)
+        self.bn = nn.BatchNorm2d(num_features=32)
+        self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout2d(p=0.02)
+
+        self.conv2_x = self._make_block(block, duplicates[0], out_channels=32, stride=1, padding=1)
+        self.conv3_x = self._make_block(block, duplicates[1], out_channels=64, stride=2, padding=1)
+        self.conv4_x = self._make_block(block, duplicates[2], out_channels=128, stride=2, padding=1)
+        self.conv5_x = self._make_block(block, duplicates[3], out_channels=256, stride=2, padding=1)
+
+        self.maxpool = nn.MaxPool2d(kernel_size=4, stride=1)
+        self.fc_layer = nn.Linear(256, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal(m.weight.data, mode='fan_out')
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
+
+
+    def _make_block(self, block, duplicates, out_channels, stride=1, padding = 1):
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = nn.Sequential(
+                conv3x3(self.in_channels, out_channels, stride=stride),
+                nn.BatchNorm2d(num_features=out_channels)
+            )
+
+        layers = []
+        layers.append(
+            block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for _ in range(1, duplicates):
+            layers.append(block(out_channels, out_channels))
+
+        return nn.Sequential(*layers)
+
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.dropout(out)
+
+        out = self.conv2_x(out)
+        out = self.conv3_x(out)
+        out = self.conv4_x(out)
+        out = self.conv5_x(out)
+
+        out = self.maxpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc_layer(out)
+
+        return out
+
+
 def calculate_accuracy(net, loader, is_gpu):
-    """
-    Calculate accuracy.
-
-    Args:
-        loader (torch.utils.data.DataLoader): training / test set loader
-        is_gpu (bool): whether to run on GPU
-
-    Returns:
-        tuple: overall accuracy
-    """
     correct = 0.
     total = 0.
 
@@ -268,73 +173,37 @@ def calculate_accuracy(net, loader, is_gpu):
 
 def train(net, criterion, optimizer, trainloader,
           testloader, start_epoch, epochs, is_gpu):
-    """
-    Training process.
-
-    Args:
-        net: ResNet model
-        criterion: CrossEntropyLoss
-        optimizer: SGD with momentum optimizer
-        trainloader: training set loader
-        testloader: test set loader
-        start_epoch: checkpoint saved epoch
-        epochs: training epochs
-        is_gpu: whether use GPU
-
-    """
-    print("==> Start training ...")
 
     for epoch in range(start_epoch, epochs + start_epoch):
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
 
-            # get the inputs
             inputs, labels = data
 
             if is_gpu:
                 inputs = inputs.cuda()
                 labels = labels.cuda()
 
-            # wrap them in Variable
             inputs, labels = Variable(inputs), Variable(labels)
 
-            # forward + backward + optimize
             outputs = net(inputs)
             loss = criterion(outputs, labels)
 
-            # zero the parameter gradients
             optimizer.zero_grad()
             loss.backward()
 
-            # optimize
             optimizer.step()
 
-            # print statistics
             running_loss += loss.data[0]
 
-        # Normalizing the loss by the total number of train batches
         running_loss /= len(trainloader)
 
-        # Calculate training/test set accuracy of the existing model
         train_accuracy = calculate_accuracy(net, trainloader, is_gpu)
         test_accuracy = calculate_accuracy(net, testloader, is_gpu)
 
         print("Iteration: {0} | Loss: {1} | Training accuracy: {2}% | Test accuracy: {3}%".format(
             epoch+1, running_loss, train_accuracy, test_accuracy))
-
-        # save model
-        if epoch % 50 == 0:
-            print('==> Saving model ...')
-            state = {
-                'net': net.module if is_gpu else net,
-                'epoch': epoch,
-            }
-            if not os.path.isdir('../checkpoint'):
-                os.mkdir('../checkpoint')
-            torch.save(state, '../checkpoint/ckpt.t7')
-
-    print('==> Finished Training ...')
 
 
 
@@ -407,7 +276,7 @@ def main():
         # Load checkpoint
         print('==> Resuming from checkpoint ...')
         assert os.path.isdir(
-            '../checkpoint'), 'Error: no checkpoint directory found!'
+            '~/checkpoint'), 'Error: no checkpoint directory found!'
         checkpoint = torch.load(args.ckptroot)
         net = checkpoint['net']
         start_epoch = checkpoint['epoch']
