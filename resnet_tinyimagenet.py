3b59d45afdbe873812051c8f1264958aa83f24c1
James Yoon <42917557+ydyoon2@users.noreply.github.com>
2019-10-15 03:00:02 -0500

    Update resnet_tinyimagenet.py

--- resnet_tinyimagenet.py
+++ resnet_tinyimagenet.py
@@ -1,37 +1,30 @@
 import torch
-import torch.utils.data
-import torchvision
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-import os
-from torch.autograd import Variable
+import torch.optim
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
+import torchvision.transforms as transforms
+import torch.utils.data
+from torch.autograd import Variable
+import os
+import torchvision.datasets as datasets
+import time
 
-
-num_epochs = 1
-batch_size = 128
-
-transform_train = transforms.Compose([
-    transforms.RandomCrop(64, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-])
-
-transform_val = transforms.Compose([
-    transforms.ToTensor(),
-])
-
+# TinyImageNet
 def create_val_folder(val_dir):
     """
-    This method is responsible for separating validation images into separate sub folders
+    This method is responsible for separating validation
+    images into separate sub folders
     """
-    path = os.path.join(val_dir, 'images')  # path where validation data is present now
-    filename = os.path.join(val_dir, 'val_annotations.txt')  # file where image2class mapping is present
-    fp = open(filename, "r")  # open file in read mode
-    data = fp.readlines()  # read line by line
-
-    # Create a dictionary with image names as key and corresponding classes as values
+    # path where validation data is present now
+    path = os.path.join(val_dir, 'images')
+    # file where image2class mapping is present
+    filename = os.path.join(val_dir, 'val_annotations.txt')
+    fp = open(filename, "r") # open file in read mode
+    data = fp.readlines() # read line by line
+    """
+    Create a dictionary with image names as key and
+    corresponding classes as values
+    """
     val_img_dict = {}
     for line in data:
         words = line.split("\t")
@@ -40,44 +33,34 @@ def create_val_folder(val_dir):
     # Create folder if not present, and move image into proper folder
     for img, folder in val_img_dict.items():
         newpath = (os.path.join(path, folder))
-        if not os.path.exists(newpath):  # check if folder exists
+        if not os.path.exists(newpath): # check if folder exists
             os.makedirs(newpath)
-        if os.path.exists(os.path.join(path, img)):  # Check if image exists in default directory
+        # Check if image exists in default directory
+        if os.path.exists(os.path.join(path, img)):
             os.rename(os.path.join(path, img), os.path.join(newpath, img))
     return
 
-# Your own directory to the train folder of tiyimagenet
-train_dir = '/u/training/tra318/scratch/tiny-imagenet-200/train/'
+
+transform_train = transforms.Compose([transforms.RandomCrop(size=64, padding=4),
+                                      transforms.RandomVerticalFlip(),
+                                      transforms.RandomHorizontalFlip(),
+                                      transforms.ToTensor() 
+                                      ])
+transform_test = transforms.Compose([transforms.ToTensor()])
+
+train_dir = '/u/training/tra318/scratch/tiny-imagenet-200/train'
 train_dataset = datasets.ImageFolder(train_dir, transform=transform_train)
-# To check the index for each classes
-# print(train_dataset.class_to_idx)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
-# Your own directory to the validation folder of tiyimagenet
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=8)
+
 val_dir = '/u/training/tra318/scratch/tiny-imagenet-200/val/'
-
-
 if 'val_' in os.listdir(val_dir+'images/')[0]:
     create_val_folder(val_dir)
     val_dir = val_dir+'images/'
 else:
     val_dir = val_dir+'images/'
+val_dataset = datasets.ImageFolder(val_dir, transform=transform_test)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=8)
 
-
-val_dataset = datasets.ImageFolder(val_dir, transform=transforms.ToTensor())
-# To check the index for each classes
-# print(val_dataset.class_to_idx)
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=8)
-
-
-# YOUR CODE GOES HERE
-# Change to your ResNet 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-for images, labels in train_loader:
-    images = Variable(images).to(device)
-    labels = Variable(labels).to(device)
-for images, labels in val_loader:
-    images = Variable(images).to(device)
-    labels = Variable(labels).to(device)
 
 # BasicBlock
 class BasicBlock(nn.Module):
@@ -120,7 +103,6 @@ class ResNet(nn.Module):
         self.bn = nn.BatchNorm2d(num_features=64)
         self.relu = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout2d(p=0.1)
-        self.avgpool = nn.AdaptiveAvgPool2d(1)
 
         self.conv2_x = self._make_block(basic_block, num_blocks[0], out_channels=64, stride=1, padding=1)
         self.conv3_x = self._make_block(basic_block, num_blocks[1], out_channels=128, stride=2, padding=1)
@@ -171,8 +153,8 @@ class ResNet(nn.Module):
 
         out = self.maxpool(out)
         out = self.avgpool(out)
-        out = out.view(out.size(0), -1)
         out = self.dropout(out)
+        out = out.view(out.size(0), -1)
         out = self.fc_layer(out)
 
         return out
@@ -180,52 +162,58 @@ class ResNet(nn.Module):
 def conv3x3(in_channels, out_channels, stride=1):
     return nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
 
-def accuracy(resnet, loader):
-    correct = 0.
-    total = 0.
 
-    for data in loader:
-        images, labels = data
-        
-        images = images.cuda()
-        labels = labels.cuda()
-        outputs = resnet(Variable(images))
-        _, predicted = torch.max(outputs.data, 1)
-
-        total += labels.size(0)
-        correct += (predicted == labels).sum()
-
-    return 100 * correct / total
-
-def train(resnet, criterion, optimizer, scheduler, train_loader, val_loader, epochs):
-    for epoch in range(epochs):
-        scheduler.step()
-        running_loss = 0.0
-        for i, data in enumerate(train_loader, 0):
-            inputs, labels = data
-            inputs = inputs.cuda()
-            labels = labels.cuda()
-            inputs, labels = Variable(inputs), Variable(labels)
-            optimizer.zero_grad()
-            outputs = resnet(inputs)
-            _, preds = torch.max(outputs, 1)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.data
-        running_loss /= len(train_loader)
-        train_accuracy = accuracy(resnet, train_loader)
-        test_accuracy = accuracy(resnet, val_loader)
-        print("epoch: {}, train_accuracy: {}%, test_accuracy: {}%".format(epoch, train_accuracy, test_accuracy))
-
-resnet = ResNet(BasicBlock, [2, 4, 4, 2], 200)
-resnet = torch.nn.DataParallel(resnet, device_ids=range(torch.cuda.device_count()))
-cudnn.benchmark = True
-
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = ResNet(BasicBlock, [2, 4, 4, 2], 200)
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(resnet.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0005)
-scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
-train(resnet, criterion, optimizer, scheduler, train_loader, val_loader, 50)
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.001)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer,step_size=5, gamma=0.2)
+
+
+#for images, labels in train_loader:
+#    images = Variable(images).to(device)
+#    labels = Variable(labels).to(device)
+#for images, labels in val_loader:
+#    images = Variable(images).to(device)
+#    labels = Variable(labels).to(device)
+
+total_step = len(train_loader)
+start_time = time.time()
+
+for epoch in range(50):
+    scheduler.step()
+    correct = 0
+    total = 0
+    for images, labels in train_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    train_accuracy = correct/total
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in val_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    test_accuracy = correct/total
+    print('Epoch {}, Loss: {:.4f}, Train Accuracy: {:.4f}, Test Accuracy: {:.4f}'
+          .format(epoch, loss.item(), train_accuracy, test_accuracy))
+    torch.save(model.state_dict(), 'epoch-{}.ckpt'.format(epoch))
+    
+    
 
 
 
@@ -233,3 +221,64 @@ train(resnet, criterion, optimizer, scheduler, train_loader, val_loader, 50)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def accuracy(resnet, loader):
+#    correct = 0.
+#    total = 0.
+#
+#    for data in loader:
+#        images, labels = data
+#        
+#        images = images.cuda()
+#        labels = labels.cuda()
+#        outputs = resnet(Variable(images))
+#        _, predicted = torch.max(outputs.data, 1)
+#
+#        total += labels.size(0)
+#        correct += (predicted == labels).sum()
+#
+#    return 100 * correct / total
+#
+#def train(resnet, criterion, optimizer, scheduler, train_loader, val_loader, epochs):
+#    for epoch in range(epochs):
+#        scheduler.step()
+#        running_loss = 0.0
+#        for i, data in enumerate(train_loader, 0):
+#            inputs, labels = data
+#            inputs = inputs.cuda()
+#            labels = labels.cuda()
+#            inputs, labels = Variable(inputs), Variable(labels)
+#            optimizer.zero_grad()
+#            outputs = resnet(inputs)
+#            _, preds = torch.max(outputs, 1)
+#            loss = criterion(outputs, labels)
+#            loss.backward()
+#            optimizer.step()
+#            running_loss += loss.data
+#        running_loss /= len(train_loader)
+#        train_accuracy = accuracy(resnet, train_loader)
+#        test_accuracy = accuracy(resnet, val_loader)
+#        print("epoch: {}, train_accuracy: {}%, test_accuracy: {}%".format(epoch, train_accuracy, test_accuracy))
+#
+#resnet = ResNet(BasicBlock, [2, 4, 4, 2], 200)
+#resnet = torch.nn.DataParallel(resnet).cuda()
+#cudnn.benchmark = True
+#criterion = nn.CrossEntropyLoss()
+#optimizer = torch.optim.SGD(resnet.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0005)
+#scheduler = torch.optim.lr_scheduler.StepLR(optimizer, gamma=0.9)
+#train(resnet, criterion, optimizer, scheduler, train_loader, val_loader, 50)
